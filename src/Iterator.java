15c052bdcb947907f65d9281266cf8f7caaa0e32
Emma Vainio <emmavainio@live.se>
2022-10-20 14:43:18 +0200

    removed variable and added trim

--- src/Iterator.java
+++ src/Iterator.java
@@ -3,10 +3,9 @@ import java.util.List;
 public class Iterator {
 
     public static Person iterateListToFindClient (String userInput, List<Person> allClients) {
-        userInput = userInput.trim();
 
         for (Person p: allClients) {
-            if (userInput.equalsIgnoreCase(p.getName()) || userInput.equalsIgnoreCase(p.getSsn()))
+            if (userInput.trim().equalsIgnoreCase(p.getName()) || userInput.trim().equalsIgnoreCase(p.getSsn()))
                 return p;
         }
         return null;
